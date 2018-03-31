b31e8b1d6f41c3605a9d4ba8f660e8939cfb3298
shoaib <shabes11@gmail.com>
2018-03-31 15:17:38 +0200

    adding startup package with Main class

---
+++ src/startup/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package startup;
+
+import view.Display;
+
+/**
+ *
+ * @author tmp-sda-1178
+ */
+public class Main {
+    
+    public static void main(String[] args){
+         
+        Display dp = new Display();
+        dp.userOptions();
+        
+    }
+}
